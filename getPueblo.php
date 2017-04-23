4f4a587d72fa1cc6a8236e20e922c87c97b9f5f7
DDHL97 <1715110005@utec-tgo.edu.mx>
2017-04-23 16:46:33 -0500

    Update getPueblo.php

--- getPueblo.php
+++ getPueblo.php
@@ -1,17 +1,17 @@
-<¿php
+<?php
+	
+	require('conexion.php');
 
-require('conexion.php');
-if(isset($_POST["id_pueblo"])){
+if(isset($_POST['id_pueblo'])){
 
-$id_pueblo = $_POST["id_pueblo"];
-$sql_query = "SELECT * FROM pueblos WHERE id_pueblo='$id_pueblo';";
-$result = mysql_query($sql_query);
+$id_pueblo = $_POST['id_pueblo'];
+$query = "SELECT * FROM pueblos WHERE id_pueblo='$id_pueblo'";
+$resultado=$mysqli->query($query);
 $rows = array();
-while($r = mysql_fetch_assoc($result)) {
+while($r = mysqli_fetch_assoc($resultado)) {
 $rows[] = $r;
 }
 print json_encode($rows);
 }else
 echo "No existe el pueblo";
-mysql_close($connection);
 ?>
