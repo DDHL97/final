d981bac609e32cca274015f0ddc4e1230fc8e13c
DDHL97 <1715110005@utec-tgo.edu.mx>
2017-04-24 09:54:08 -0500

    Add files via upload

---
+++ getLugares.php
@@ -0,0 +1,17 @@
+﻿<?php
+	
+	require('conexion.php');
+
+if(isset($_POST['id_pueblo'])){
+
+$id_pueblo = $_POST['id_pueblo'];
+$query = "SELECT * FROM lugares_interes WHERE id_pueblo='$id_pueblo'";
+$resultado=$mysqli->query($query);
+$rows = array();
+while($r = mysqli_fetch_assoc($resultado)) {
+$rows[] = $r;
+}
+print json_encode($rows);
+}else
+echo "No existe el lugar";
+?>
